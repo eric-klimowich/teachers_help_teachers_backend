5e532373a2014b14804a7e6e4cea8d64798dfd90
eric-klimowich <eklimowich@hotmail.com>
2019-06-19 12:29:08 -0400

    Made an error in updating user controller, fixed create and update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,17 +9,17 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: @user, status: :accepted
+      render json: { user: UserSerializer.new(@user) }, status: :created
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+      render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
 
   def update
     if @user.update(user_params)
-      render json: { user: UserSerializer.new(@user) }, status: :created
+      render json: @user, status: :accepted
     else
-      render json: { error: 'failed to create user' }, status: :not_acceptable
+      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
     end
   end
 
@@ -30,7 +30,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :first_name, :last_name, :grade_teaching, :subject_teaching)
+    params.require(:user).permit(:username, :first_name, :last_name, :grade_teaching, :subject_teaching)
   end
 
   def find_user
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, :grade_levels, :grade_subjects, :lessons, :comments, :favorite_lessons
+      resources :users, only: %i[create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+      resources :grade_levels, :grade_subjects, :lessons, :comments, :favorite_lessons
     end
   end
 end
