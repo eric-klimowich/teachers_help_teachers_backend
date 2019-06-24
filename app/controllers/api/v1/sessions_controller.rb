68b3dd500cf0af593e8e26489e42858145f5510c
eric-klimowich <eklimowich@hotmail.com>
2019-06-24 18:15:24 -0400

    Added payload, secret key, and token assignments to sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,10 +4,21 @@ class Api::V1::SessionsController < ApplicationController
     @user = User.find_by(username: params["username"])
 
     if (@user && @user.authenticate(params["password"]))
+      payload = {
+        sub: @user.id,
+        iat: Time.now.to_i,
+        exp: Time.now.to_i + 7200000
+      }
+
+      secret_key = "password"
+
+      token = JWT.encode payload, secret_key, "HS256"
+
       render json: {
         username: @user.username,
-        id: @user.id
-      }
+        id: @user.id,
+        token: token
+        }
     else
       render json: {
         errors: "Those credentials don't match anything we have in our database"
